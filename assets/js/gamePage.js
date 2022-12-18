8e832507aa9c5c8a503755876b03351ade2026bc
Zasada94 <pawelzasada2@wp.pl>
2022-12-18 13:44:27 +0100

    inner wall array added, collision detenction working. gotta change to collision detection in every direction to make it work

--- assets/js/gamePage.js
+++ assets/js/gamePage.js
@@ -1,4 +1,4 @@
-import heartImage from './../img/heart.png';
+import heartImage from "./../img/heart.png";
 
 const gamePage = () => {
 	app.innerHTML = `
@@ -31,7 +31,7 @@ const gamePage = () => {
 		let mins = Math.floor(seconds / 60);
 		let secs = Math.floor(seconds % 60);
 		timeCounter.innerHTML =
-		mins.toString().padStart(2, "0") + ":" + secs.toString().padStart(2, "0");
+			mins.toString().padStart(2, "0") + ":" + secs.toString().padStart(2, "0");
 	};
 
 	const showTime = () => {
@@ -40,13 +40,11 @@ const gamePage = () => {
 			setInterval(gameTimer, 1000);
 		}
 	};
-
-	let height;
-	let width;
-	let windowWidth;
-	let windowHeight;
-	let tile = canvas.width / 15;
-
+	let height = canvas.offsetHeight;
+	let width = canvas.offsetWidth;
+	let windowWidth = body.offsetWidth;
+	let windowHeight = body.offsetHeight;
+	let tile = width / 15;
 	const player1 = {
 		speed: 1,
 		width: tile,
@@ -59,6 +57,61 @@ const gamePage = () => {
 		y: tile,
 	};
 
+	const CanvasResize = () => {
+		width = canvas.offsetWidth;
+		height = canvas.offsetHeight;
+		windowWidth = body.offsetWidth;
+		windowHeight = body.offsetHeight;
+		canvas.setAttribute("height", height);
+		canvas.setAttribute("width", width);
+		tile = width / 15;
+		player1.width = tile;
+		player1.height = tile;
+		player1.y = tile;
+		player1.x = tile;
+	};
+	CanvasResize();
+	window.addEventListener("resize", CanvasResize);
+
+	const innerWalls = [
+		{
+			x: 2 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+		{
+			x: 4 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+		{
+			x: 6 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+		{
+			x: 8 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+		{
+			x: 10 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+		{
+			x: 12 * tile,
+			y: 2 * tile,
+			width: tile,
+			height: tile,
+		},
+	];
+
 	const player1Image = new Image();
 	player1Image.src = player1.image;
 
@@ -70,22 +123,6 @@ const gamePage = () => {
 
 	const wallImage = new Image();
 	wallImage.src = wall.image;
-
-	const CanvasResize = () => {
-		width = canvas.offsetWidth;
-		height = canvas.offsetHeight;
-		windowWidth = body.offsetWidth;
-		windowHeight = body.offsetHeight;
-		canvas.setAttribute("height", height);
-		canvas.setAttribute("width", width);
-		tile = canvas.width / 15;
-		player1.width = tile;
-		player1.height = tile;
-		player1.y = tile;
-		player1.x = tile;
-	};
-	CanvasResize();
-	window.addEventListener("resize", CanvasResize);
 
 	const ctx = canvas.getContext("2d");
 
@@ -102,7 +139,7 @@ const gamePage = () => {
 			a.y < b.y + b.height &&
 			a.y + a.height > b.y;
 		if (boo) {
-			// console.log("HIT");
+			console.log("HIT");
 		}
 		return boo;
 	}
@@ -146,25 +183,34 @@ const gamePage = () => {
 		// if (time.textContent.includes("00:00")) {
 		// 	gameOver();
 		// }
-		for (let i = 1; i < 6; i++) {
-			if (keyz.ArrowLeft && player1.x > tile) {
-				if (player1.y + tile < 2 * tile) {
+		if (
+			col(player1, innerWalls[0]) == false &&
+			col(player1, innerWalls[1]) == false &&
+			col(player1, innerWalls[2]) == false &&
+			col(player1, innerWalls[3]) == false &&
+			col(player1, innerWalls[4]) == false &&
+			col(player1, innerWalls[5]) == false
+		) {
+			for (let i = 1; i < 6; i++) {
+				if (keyz.ArrowLeft && player1.x > tile) {
+					// if (player1.y + tile < 2 * tile) {
 					player1.x -= player1.speed;
+					// }
 				}
-			}
-			if (keyz.ArrowLeft && player1.x > tile) {
-				if (player1.y > 3 * tile) {
+				if (keyz.ArrowLeft && player1.x > tile) {
+					// if (player1.y > 3 * tile) {
 					player1.x -= player1.speed;
+					// }
 				}
-			}
-			if (keyz.ArrowRight && player1.x < tile * 13) {
-				player1.x += player1.speed;
-			}
-			if (keyz.ArrowUp && player1.y > tile) {
-				player1.y -= player1.speed;
-			}
-			if (keyz.ArrowDown && player1.y < tile * 13) {
-				player1.y += player1.speed;
+				if (keyz.ArrowRight && player1.x < tile * 13) {
+					player1.x += player1.speed;
+				}
+				if (keyz.ArrowUp && player1.y > tile) {
+					player1.y -= player1.speed;
+				}
+				if (keyz.ArrowDown && player1.y < tile * 13) {
+					player1.y += player1.speed;
+				}
 			}
 		}
 		ctx.drawImage(
@@ -188,6 +234,7 @@ const gamePage = () => {
 			ctx.drawImage(wallImage, 2 * i * tile, 10 * tile, tile, tile);
 			ctx.drawImage(wallImage, 2 * i * tile, 12 * tile, tile, tile);
 		}
+
 		if (game.play) {
 			game.req = requestAnimationFrame(draw);
 		}
